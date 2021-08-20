dd2e12f91425804f690181c2c6a64e99ca250f7d
muazzamazaz <muazzamazaz@hotmail.com>
2021-08-20 18:37:50 +0500

    test

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+Use App\Photo;
+class FileUploadController extends Controller
+{
+    // function to store file in 'upload' folder
+    public function fileStore(Request $request)
+    {
+        $upload_path = public_path('upload');
+        $file_name = $request->file->getClientOriginalName();
+        $generated_new_name = time() . '.' . $request->file->getClientOriginalExtension();
+        $request->file->move($upload_path, $generated_new_name);
+         
+        $insert['title'] = $file_name;
+        $check = Photo::insertGetId($insert);
+        return response()->json(['success' => 'You have successfully uploaded "' . $file_name . '"']);
+    }
+}
